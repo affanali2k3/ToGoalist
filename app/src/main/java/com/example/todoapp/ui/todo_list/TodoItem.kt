a927d53caf33c470f7729c3f0673c08fcf38f65f
affanali2k3 <affanalikhanxx@gmail.com>
2023-06-12 21:26:37 +0500

    Homepage UI changed

--- app/src/main/java/com/example/todoapp/ui/todo_list/TodoItem.kt
+++ app/src/main/java/com/example/todoapp/ui/todo_list/TodoItem.kt
@@ -1,50 +1,59 @@
 package com.example.todoapp.ui.todo_list
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import com.example.todoapp.data.Todo
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun TodoItem(todo: Todo, onEvent: (TodoListEvent) -> Unit, modifier: Modifier){
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically){
-        Column(
-            modifier=modifier.weight(1f),
-            verticalArrangement = Arrangement.Center
-        ){
-            Row(verticalAlignment = Alignment.CenterVertically){
-                Text(text = todo.title, fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.width(8.dp))
-                IconButton(onClick = {onEvent(TodoListEvent.OnDeleteTodoClick(todo))}) {
-                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
-                    
-                }
-                
-                
-            }
-            todo.description?.let {
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = todo.description)
+fun TodoItem(todo: Todo, onEvent: (TodoListEvent) -> Unit, modifier: Modifier) {
+    Surface(
+        modifier = Modifier.shadow(
+            elevation = 4.dp,
+            shape = RoundedCornerShape(4.dp),
+            clip = true
+        )
+    ) {
+        Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+            Column(
+                modifier = modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = todo.title, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    IconButton(onClick = { onEvent(TodoListEvent.OnDeleteTodoClick(todo)) }) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
 
+                    }
+
+
+                }
+                todo.description?.let {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = todo.description)
+
+                }
             }
+
+            Checkbox(checked = todo.isDone, onCheckedChange = { isChecked ->
+                onEvent(
+                    TodoListEvent.OnDoneTodoClick(
+                        todo,
+                        isChecked
+                    )
+                )
+            })
         }
-        
-        Checkbox(checked = todo.isDone, onCheckedChange = {isChecked -> onEvent(
-            TodoListEvent.OnDoneTodoClick(
-                todo,
-                isChecked
-            )
-        )})
     }
 }
--- app/src/main/java/com/example/todoapp/ui/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/todo_list/TodoListScreen.kt
@@ -1,18 +1,27 @@
 package com.example.todoapp.ui.todo_list
 
+import android.widget.ProgressBar
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mynewapp.util.UiEvent
@@ -48,25 +57,89 @@ fun TodoListScreen(
         }
 
     }) { padding ->
-        LazyColumn(modifier = Modifier
-            .padding(padding)
-            .fillMaxHeight()) {
-            items(todos.value) { todo ->
-                TodoItem(
-                    todo = todo,
-                    onEvent = viewModel::onEvent,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                        .clickable {
-                            viewModel.onEvent(
-                                TodoListEvent.OnViewTodoClick(todo)
-                            )
-                        })
+        Column(modifier = Modifier.fillMaxHeight().padding(horizontal = 10.dp)) {
+
+            topGoalsWidget()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+            ) {
+                items(todos.value) { todo ->
+                    TodoItem(
+                        todo = todo,
+                        onEvent = viewModel::onEvent,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .clickable {
+                                viewModel.onEvent(
+                                    TodoListEvent.OnViewTodoClick(todo)
+                                )
+                            })
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
+                
+
+            }
+        }
+
+    }
+
+
+}
+
+@Composable
+fun topGoalsWidget() {
+    Surface(
+        modifier = Modifier
+            .padding(all = 10.dp)
+            .height(120.dp)
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(4.dp),
+                clip = true
+            ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(modifier = Modifier.fillMaxHeight()) {
+                Icon(
+                    modifier = Modifier.size(100.dp),
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Avatar"
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                progressIndicator(color = Color.Blue, progress = 0.7f)
+                progressIndicator(color = Color.Cyan, progress = 0.2f)
+                progressIndicator(color = Color.Green, progress = 0.9f)
+                progressIndicator(color = Color.Red, progress = 1f)
+                progressIndicator(color = Color.Yellow, progress = 0.1f)
+
             }
 
         }
     }
+}
 
-
+@Composable
+fun progressIndicator(color: Color, progress: Float) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        LinearProgressIndicator(
+            modifier = Modifier.width(150.dp),
+            progress = progress,
+            color = color,
+            backgroundColor = Color.LightGray,
+        )
+        Spacer(modifier = Modifier.width(width = 10.dp))
+        Text(text = "${(progress * 100).toInt()}%", fontSize = TextUnit(10f, TextUnitType.Sp))
+    }
 }
