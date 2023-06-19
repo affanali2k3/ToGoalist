f8db79242984e49b229f7bba35b34208450bbc3b
affanali2k3 <affanalikhanxx@gmail.com>
2023-06-19 16:05:32 +0500

    Added date picker

--- app/src/main/java/com/example/todoapp/ui/todo_list/views/widgets/AddTodoDialog.kt
+++ app/src/main/java/com/example/todoapp/ui/todo_list/views/widgets/AddTodoDialog.kt
@@ -1,18 +1,33 @@
 package com.example.todoapp.ui.todo_list.views
 
+import android.app.DatePickerDialog
+import android.os.Build
+import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todoapp.ui.todo_list.TodoListViewModel
+import com.example.todoapp.ui.todo_list.views.widgets.SelectGoalDropDown
+import java.time.LocalDate
+import java.util.*
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTodoDialog(
 ) {
@@ -23,9 +38,14 @@ fun AddTodoDialog(
     val description = remember {
         mutableStateOf("")
     }
+    val categoryPoints = remember {
+        mutableStateOf("")
+    }
     var showMenu = remember { mutableStateOf(false) }
     val openDialog by viewModel.addTodoDialogOpen
-
+    val date = remember {
+        mutableStateOf(LocalDate.now())
+    }
 
     if (openDialog) {
         AlertDialog(
@@ -33,7 +53,7 @@ fun AddTodoDialog(
                 viewModel.addTodoDialogOpen.value = false
             },
             text = {
-                Column() {
+                Column {
                     OutlinedTextField(
                         value = title.value,
                         onValueChange = { newValue ->
@@ -50,68 +70,32 @@ fun AddTodoDialog(
                         placeholder = { Text(text = "Description") }
                     )
                     Spacer(modifier = Modifier.height(8.dp))
-                    Row() {
-                        SelectGoalDropDown()
-                        Box(modifier = Modifier.width(10.dp).background(color = Color.Red))
+                    SelectGoalDropDown(label = "Category")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OutlinedTextField(
+                        value = categoryPoints.value,
+                        onValueChange = { newValue ->
+                            categoryPoints.value = newValue
+                        },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        placeholder = { Text(text = "Points") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    SelectGoalDropDown(label = "Priority")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(
+                        modifier = Modifier.clickable(
+                            onClick = {
+                            }
+                        )
+                    ) {
+                        Text(text = "Date")
+                        Text(text = date.toString())
                     }
                 }
             }, buttons = {})
     }
 
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun SelectGoalDropDown() {
-    val listItems = arrayOf("Favorites", "Options", "Settings", "Share")
-    val contextForToast = LocalContext.current.applicationContext
-    // state of the menu
-    var expanded by remember {
-        mutableStateOf(false)
-    }
-    // remember the selected item
-    var selectedItem by remember {
-        mutableStateOf(listItems[0])
-    }
-    // box
-    ExposedDropdownMenuBox(
-        expanded = expanded,
-        onExpandedChange = {
-            expanded = !expanded
-        }
-    ) {
-        // text field
-        TextField(
-            value = selectedItem,
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = "Label") },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            },
-            colors = ExposedDropdownMenuDefaults.textFieldColors()
-        )
-        // menu
-        ExposedDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
-            // this is a column scope
-            // all the items are added vertically
-            listItems.forEach { selectedOption ->
-                // menu item
-                DropdownMenuItem(onClick = {
-                    selectedItem = selectedOption
-                    Toast.makeText(contextForToast, selectedOption, Toast.LENGTH_SHORT).show()
-                    expanded = false
-                }) {
-                    Text(text = selectedOption)
-                }
-            }
-        }
-    }
 }
 
 
---
+++ app/src/main/java/com/example/todoapp/ui/todo_list/views/widgets/SelectGoalDropDown.kt
@@ -0,0 +1,62 @@
+package com.example.todoapp.ui.todo_list.views.widgets
+
+import android.widget.Toast
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SelectGoalDropDown(label: String) {
+    val listItems = arrayOf("Favorites", "Options", "Settings", "Share")
+    val contextForToast = LocalContext.current.applicationContext
+    // state of the menu
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    // remember the selected item
+    var selectedItem by remember {
+        mutableStateOf(listItems[0])
+    }
+    // box
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        // text field
+        TextField(
+            value = selectedItem,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        // menu
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            // this is a column scope
+            // all the items are added vertically
+            listItems.forEach { selectedOption ->
+                // menu item
+                DropdownMenuItem(onClick = {
+                    selectedItem = selectedOption
+                    Toast.makeText(contextForToast, selectedOption, Toast.LENGTH_SHORT).show()
+                    expanded = false
+                }) {
+                    Text(text = selectedOption)
+                }
+            }
+        }
+    }
+}
+
+
